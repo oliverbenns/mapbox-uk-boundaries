245514fb936b50810362f8bd5aa9a22ea571f261
Oliver Benns <oliverbenns@gmail.com>
2023-08-26 15:14:44 +0100

    x

--- src/App.tsx
+++ src/App.tsx
@@ -30,42 +30,7 @@ function App() {
         console.log("data", data);
 
         // Data is in EPSG:27700 (British National Grid) format and Mapbox requires it in EPSG:4326.
-        proj4.defs(
-          "EPSG:27700",
-          "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs"
-        );
-
-        data.features.forEach((feature, i) => {
-          feature.geometry.coordinates.forEach((coord, j) => {
-            coord.forEach((pos, k) => {
-              // safety
-              if (pos.length === 0) {
-                return;
-              }
-
-              // Pos is typed as number[] but sometimes it's number[][].
-              // It looks like this could come from an old Geojson spec.
-              const isLegacyGeoJson = Array.isArray(pos[0]);
-              if (isLegacyGeoJson) {
-                pos.forEach((innerPos, l) => {
-                  const newVal = proj4(
-                    "EPSG:27700",
-                    "EPSG:4326",
-                    innerPos as never as number[]
-                  );
-
-                  (data.features[i].geometry.coordinates[j][k][
-                    l
-                  ] as never as number[]) = newVal;
-                });
-                return;
-              }
-              const newVal = proj4("EPSG:27700", "EPSG:4326", pos);
-
-              data.features[i].geometry.coordinates[j][k] = newVal;
-            });
-          });
-        });
+        normalizeData(data);
 
         if (!map.current) {
           throw new Error("could not get map ref");
@@ -111,5 +76,50 @@ function App() {
     </div>
   );
 }
+
+// Converts polygon boundary coordinates from EPSG:27700 to EPSG:4326.
+// NOTE: This mutates!!
+const normalizeData = (data: FeatureCollection<Polygon>) => {
+  proj4.defs(
+    "EPSG:27700",
+    "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs"
+  );
+
+  // Delete the crs header (not sure why untyped) that states EPSG:27700
+  // incase Mapbox decides to respect this later.
+  delete (data as any).crs;
+
+  data.features.forEach((feature, i) => {
+    feature.geometry.coordinates.forEach((coord, j) => {
+      coord.forEach((pos, k) => {
+        // safety
+        if (pos.length === 0) {
+          return;
+        }
+
+        // Pos is typed as number[] but sometimes it's number[][].
+        // It looks like this could come from an old Geojson spec.
+        const isLegacyGeoJson = Array.isArray(pos[0]);
+        if (isLegacyGeoJson) {
+          pos.forEach((innerPos, l) => {
+            const newVal = proj4(
+              "EPSG:27700",
+              "EPSG:4326",
+              innerPos as never as number[]
+            );
+
+            (data.features[i].geometry.coordinates[j][k][
+              l
+            ] as never as number[]) = newVal;
+          });
+          return;
+        }
+        const newVal = proj4("EPSG:27700", "EPSG:4326", pos);
+
+        data.features[i].geometry.coordinates[j][k] = newVal;
+      });
+    });
+  });
+};
 
 export default App;
