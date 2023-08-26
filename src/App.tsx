fe8079f27c6bee33a47312819b5596f6f3e1079a
Oliver Benns <oliverbenns@gmail.com>
2023-08-26 15:09:01 +0100

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+import proj4 from "proj4";
+import { FeatureCollection, Polygon } from "geojson";
 
 function App() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
@@ -21,11 +23,84 @@ function App() {
       },
     });
 
-    (async () => {
-      const res = await fetch("./boundaries.geojson");
-      const data = await res.json();
-      console.log("data", data);
-    })();
+    map.current.on("load", () => {
+      (async () => {
+        const res = await fetch("./boundaries.geojson");
+        const data = (await res.json()) as FeatureCollection<Polygon>;
+        console.log("data", data);
+
+        // Data is in EPSG:27700 (British National Grid) format and Mapbox requires it in EPSG:4326.
+        proj4.defs(
+          "EPSG:27700",
+          "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs"
+        );
+
+        data.features.forEach((feature, i) => {
+          feature.geometry.coordinates.forEach((coord, j) => {
+            coord.forEach((pos, k) => {
+              // safety
+              if (pos.length === 0) {
+                return;
+              }
+
+              // Pos is typed as number[] but sometimes it's number[][].
+              // It looks like this could come from an old Geojson spec.
+              const isLegacyGeoJson = Array.isArray(pos[0]);
+              if (isLegacyGeoJson) {
+                pos.forEach((innerPos, l) => {
+                  const newVal = proj4(
+                    "EPSG:27700",
+                    "EPSG:4326",
+                    innerPos as never as number[]
+                  );
+
+                  (data.features[i].geometry.coordinates[j][k][
+                    l
+                  ] as never as number[]) = newVal;
+                });
+                return;
+              }
+              const newVal = proj4("EPSG:27700", "EPSG:4326", pos);
+
+              data.features[i].geometry.coordinates[j][k] = newVal;
+            });
+          });
+        });
+
+        if (!map.current) {
+          throw new Error("could not get map ref");
+        }
+
+        map.current.addSource("boundaries", {
+          type: "geojson",
+          data: data,
+        });
+
+        // Add a new layer to visualize the polygon.
+        map.current.addLayer({
+          id: "boundaries_fill",
+          type: "fill",
+          source: "boundaries", // reference the data source
+          layout: {},
+          paint: {
+            "fill-color": "#0080ff", // blue color fill
+            "fill-opacity": 0.5,
+          },
+        });
+
+        // Add a black outline around the polygon.
+        map.current.addLayer({
+          id: "boundaries_line",
+          type: "line",
+          source: "boundaries",
+          layout: {},
+          paint: {
+            "line-color": "#000",
+            "line-width": 3,
+          },
+        });
+      })();
+    });
   });
 
   return (
