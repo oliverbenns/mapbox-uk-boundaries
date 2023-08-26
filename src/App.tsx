e7b31be00173527bcfa948363a09981efa766288
Oliver Benns <oliverbenns@gmail.com>
2023-08-26 17:19:15 +0100

    x

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import mapboxgl from "mapbox-gl";
 import proj4 from "proj4";
 import { FeatureCollection, Polygon } from "geojson";
 
+interface CountyProperties {
+  GlobalID: string;
+  OBJECTID: number;
+  bng_e: number;
+  bng_n: number;
+  ctyua19cd: string;
+  ctyua19nm: string;
+  ctyua19nmw: string;
+  lat: number;
+  long: number;
+}
+
+type BoundaryData = FeatureCollection<Polygon, CountyProperties>;
+
 function App() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
@@ -15,7 +29,7 @@ function App() {
     map.current = new mapboxgl.Map({
       container: mapContainer.current as HTMLElement,
       accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: "mapbox://styles/mapbox/light-v11",
       center: [-3.4735, 54.1171],
       zoom: 4,
       projection: {
@@ -24,9 +38,16 @@ function App() {
     });
 
     map.current.on("load", () => {
+      if (!map.current) {
+        throw new Error("could not get map ref");
+      }
+
+      removeLabels(map.current);
+
       (async () => {
         const res = await fetch("./boundaries.geojson");
-        const data = (await res.json()) as FeatureCollection<Polygon>;
+        const data = (await res.json()) as BoundaryData;
+        console.log("data", data);
 
         // Data is in EPSG:27700 (British National Grid) format and Mapbox requires it in EPSG:4326.
         normalizeData(data);
@@ -54,6 +75,16 @@ function App() {
     </div>
   );
 }
+
+// Remove programmatically so no need to create a new style in mapbox studio.
+// Does result in flash though on initial load.
+const removeLabels = (map: mapboxgl.Map) => {
+  map.getStyle().layers.forEach((l) => {
+    if (l.type == "symbol") {
+      map.setLayoutProperty(l.id, "visibility", "none");
+    }
+  });
+};
 
 const addLayers = (map: mapboxgl.Map) => {
   map.addLayer({
@@ -86,19 +117,34 @@ const addLayers = (map: mapboxgl.Map) => {
 
 const addHoverListeners = (map: mapboxgl.Map) => {
   let hoveredFeatureId: string | number | undefined;
+  const popup = new mapboxgl.Popup({
+    offset: [0, -8],
+    closeButton: false,
+    closeOnClick: false,
+  });
 
-  map.on("mousemove", "boundaries-fill", (e) => {
-    if (!e.features || e.features.length === 0) {
-      return;
-    }
-
+  map.on("mousemove", "boundaries-fill", (ev) => {
     if (hoveredFeatureId !== undefined) {
       map.setFeatureState(
         { source: "boundaries", id: hoveredFeatureId },
         { hover: false }
       );
     }
-    hoveredFeatureId = e.features[0].id;
+
+    if (!ev.features || ev.features.length === 0) {
+      return;
+    }
+
+    const feature = ev.features[0];
+    const properties = feature.properties as CountyProperties;
+    const countyName = properties.ctyua19nm;
+
+    popup
+      .setLngLat(ev.lngLat)
+      .setHTML("<span>" + countyName + "</span>")
+      .addTo(map);
+
+    hoveredFeatureId = feature.id;
     map.setFeatureState(
       { source: "boundaries", id: hoveredFeatureId },
       { hover: true }
@@ -106,6 +152,8 @@ const addHoverListeners = (map: mapboxgl.Map) => {
   });
 
   map.on("mouseleave", "boundaries-fill", () => {
+    popup.remove();
+
     if (hoveredFeatureId !== undefined) {
       map.setFeatureState(
         { source: "boundaries", id: hoveredFeatureId },
@@ -118,7 +166,7 @@ const addHoverListeners = (map: mapboxgl.Map) => {
 
 // Converts polygon boundary coordinates from EPSG:27700 to EPSG:4326.
 // NOTE: This mutates!!
-const normalizeData = (data: FeatureCollection<Polygon>) => {
+const normalizeData = (data: BoundaryData) => {
   proj4.defs(
     "EPSG:27700",
     "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs"
